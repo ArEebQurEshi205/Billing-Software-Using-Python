8fdfc1e616760fd35613a24662239f38bb1547ff
ArEeb QurEshi <areebulameenq12345@gmail.com>
2024-01-04 23:40:55 +0500

    adding the bill recipt

--- main.py
+++ main.py
@@ -12,37 +12,38 @@ class Bill_App:
         # ----------- Image Section -----------
         # Image 1
         img=Image.open("image/frontImage1.png")
-        img=img.resize((400,110),Image.AFFINE)
+        img=img.resize((400,100),Image.AFFINE)
         self.photoimg=ImageTk.PhotoImage(img)
         lbl_img=Label(self.root,image=self.photoimg)
-        lbl_img.place(x=0,y=0,width=400,height=110)
+        lbl_img.place(x=0,y=0,width=400,height=100)
         
         # Image 2
         img_1=Image.open("image/frontImage2.png")
-        img_1=img_1.resize((620,110),Image.AFFINE)
+        img_1=img_1.resize((620,100),Image.AFFINE)
         self.photoimg_1=ImageTk.PhotoImage(img_1)
         lbl_img_1=Label(self.root,image=self.photoimg_1)
-        lbl_img_1.place(x=380,y=0,width=620,height=110)
+        lbl_img_1.place(x=380,y=0,width=620,height=100)
 
         # Image 3
         img_2=Image.open("image/frontImage3.png")
-        img_2=img_2.resize((400,110),Image.AFFINE)
+        img_2=img_2.resize((400,100),Image.AFFINE)
         self.photoimg_2=ImageTk.PhotoImage(img_2)
         lbl_img_2=Label(self.root,image=self.photoimg_2)
-        lbl_img_2.place(x=1000,y=0,width=400,height=110)
+        lbl_img_2.place(x=1000,y=0,width=400,height=100)
 
 
         # ------------ Title Section --------
 
         lbl_title=Label(self.root,text="BILLING SOFTWARE",font=("Arial",26,"bold","italic"),bg="black",fg="white")
-        lbl_title.place(x=0,y=112,width=1400,height=45)
+        lbl_title.place(x=0,y=103,width=1400,height=45)
 
-        Main_Frame=Frame(self.root,bd=5,relief=GROOVE,bg="white")
-        Main_Frame.place(x=0,y=159,width=1400,height=537)
+        # ------- details frame --------
+        Main_Frame=Frame(self.root,bd=9,relief=GROOVE,bg="white")
+        Main_Frame.place(x=0,y=150,width=1360,height=550)
 # ---------------------------------------------------------------------------------------------------------#
         # ------- Customer Frame --------
         Cust_Frame=LabelFrame(Main_Frame,text="Customer Details:",font=("Arial Black",12,"bold"),bg="Black",fg="white",bd=8)
-        Cust_Frame.place(x=2,y=1,width=350,height=130)
+        Cust_Frame.place(x=-1,y=3,width=350,height=130)
        
         # -------- Mobile Info Section ---------
         self.lbl_mob=Label(Cust_Frame,text="Mobile Number:",font=("arial",12,"bold"),bg="black",fg="white")
@@ -62,7 +63,7 @@ class Bill_App:
 # ---------------------------------------------------------------------------------------------------------#
         # ------- Product Frame --------
         Product_Frame=LabelFrame(Main_Frame,text="Product:",font=('Arial Black',13,'bold'),bg="black",fg="white",bd=8)
-        Product_Frame.place(x=355,y=1,width=560,height=130)
+        Product_Frame.place(x=351,y=3,width=560,height=130)
 
         # ------- Product Category -------
         self.lblCategory=Label(Product_Frame,font=('arial',12,'bold'),bg="black",fg="white",text="Select Category:",bd=4)
@@ -98,15 +99,18 @@ class Bill_App:
 
 #-----------------------------------------------------------------------------------------------------------
         # ---------- Right Frame Bill Section ----------------
-        RighLabelFrame=LabelFrame(Main_Frame,text="Bill Section",font=("Arial Black",14,"bold"),bg="black",fg="white",bd=8)
-        RighLabelFrame.place(x=920,y=45,width=427,height=440)
+        RighLabelFrame=LabelFrame(Main_Frame,text="Bill Section",font=("Arial Black",14,"bold"),bg="black",fg="white",bd=6)
+        RighLabelFrame.place(x=915,y=45,width=423,height=342)
 
         scroll_y=Scrollbar(RighLabelFrame,orient=VERTICAL)
-        self.textarea=Text(RighLabelFrame,yscrollcommand=scroll_y.set,bg="black",fg="white",font=("Arial Black",12,"bold"))
+        self.textarea=Text(RighLabelFrame,yscrollcommand=scroll_y.set,bg="black",fg="white",font=("Arial Black",10,"bold"))
         scroll_y.pack(side=RIGHT,fill=Y)
         scroll_y.config(command=self.textarea.yview)
         self.textarea.pack(fill=BOTH,expand=1)
 
+        # ------------- Bill Counter  Section --------------
+        Bottom_Frame=LabelFrame(Main_Frame,text="Bill Recipt",font=("Arial Black",14,"bold"),bg="black",fg="white")
+        Bottom_Frame.place(x=0,y=390,width=1337,height=139)
 
 
 
